e5ca53af035d65896137834a474dc186457ca1d3
EC2 Default User <ec2-user@ip-172-31-23-62.ec2.internal>
2019-09-11 14:46:44 +0000

    initial commit

--- 1.mapping-time/process/README.md
+++ 1.mapping-time/process/README.md
@@ -1,3 +1,39 @@
-## Process
+##Process
 
-Description of the contents of this folder and the logic of your data → retinal variables mapping.
+I have made 3 sketches of how to map time using components learned from the 
+Bertin Retinal Variables excercise.
+
+###Concept 1: Synesthete's Clock
+I am a person who experiences grapheme synesthesia, which essentially means I 
+percieve numbers and letters to have specific colors. I decided to play with 
+this association for concept 1. I am using the retinal variable of color to 
+map changes of seconds, minutes and hours. The time is represented in 6 colored 
+bars, from left to right like a typical digital display, the first two bars are 
+the hour, the middle two bars are the minutes, and the right most two bars are 
+the seconds. The clock is in miliary time, so there is no need to otherwise 
+differentiate between AM and PM. The "key" is the colors I percieve for each 
+number (0-9). This means that the clock is like a secret code, and useless to 
+anyone else. 
+
+
+###Concept 2: Cat Eye Clock
+As a cat lover, I have stared lovingly into cat eyes enough to know their pupils 
+change size dramatically throughout the day; most often when calm, playful, or 
+when "hunting". For this clock, my intent is to use the retinal variable of size 
+to differentiate time. The cat eye pupil is largest at midnight, and gradially 
+becomes smaller by noon, and grows gradually larger again in the afternoon to 
+be its fullest again by midnight. This clock is meant to give you a small hint 
+at the time, but not to give you much useful detail. It's aloof, like a real cat.
+
+
+###Concept 3: Rube Goldberg Clock
+This clock uses position, size and also literal quantity to map time. The idea 
+is to have three stacked bars that are all slightly angled to give an illusion 
+of one tipping onto another like a Rube Goldberg machine. The top bar shows 
+seconds, middle is minutes, and the bottom one is hours. Each second, a small 
+circle is added to the bar on the right edge, when 59 seconds have passes, a 
+medium size circle appears to "drop" onto the middle bar representing a minute. 
+Eventually, when 59 minutes have passed, a larger circle "drops" onto the hour 
+bar. You would be able to tell the time by counting the hour circles (up to 24),
+the minute circles, and the second circles. 
+
--- 1.mapping-time/project/sketch.js
+++ 1.mapping-time/project/sketch.js
@@ -1,29 +1,115 @@
+// CODE BELOW NOT COMPLETE!
 
 function setup() {
   // set the width & height of the sketch
-  createCanvas(400, 130)
+  createCanvas(600, 300);
+  frameRate(60);
+  background(255)
+  noStroke()
 
-  // print the time to the console once at the beginning of the run. try opening up the
-  // web inspector and poking around to see the various values the clock function gives you
-  print('starting time:', clock())
+print('starting time:', clock())
+  
+  
+// create bars for hours/minutes/seconds
+var barHeight = 400 // height of each bar
+var maxWidth = 200 // maximum width of each bar (the actual width will always be ≤ this)
+var spacing = 0 // the vertical space to skip between bars
+var discrete = false
+var colors = {};
+  colors.key0 = "white";
+  colors.key1 = "darkblue";
+  colors.key2 = "yellow";
+  colors.key3 = "green";
+  colors.key4 = "red";
+  colors.key5 = "orange";
+  colors.key6 = "darkpink";
+  colors.key7 = "lightpurple";
+  colors.key8 = "darkred";
+  colors.key9 = "darkpurple"
+};
 
+  for (var i = step; i <= width; i += step) {
+    // set the color by mapping `i` from x-axis coordinates to 0-255 grey values
+    stroke(map(i, 0,width, 0,255))
+
+    if (i < middle) {
+      leftOfCenter = true;
+    } else {
+      leftOfCenter = false;
+    }
+
+
+function clock(){
+  let t = now(),
+
+for (hours:t.hour()) {
+  draw(rect(0, 0, 100, 300))
+
+  if (hour == 0) {
+    fill(key0);
+  }  
+  if (hour == 1) {
+    fill(key1);
+  }
+  if (hour == 2) {
+    fill(key1);
+    
+  }
+      ;
 }
+if (hours:t.hour() == 1) {
+      fill(key1);
+      rect(0, 0, 100, 300);
+    }
+    
+        if (i < middle) {
+      leftOfCenter = true;
+    } else {
+  var now = clock()
+  if (discrete){
+    if hour
+    var hourFill1 = fill(now.hour, 'darkslategrey')
+    // the map() function lets us *normalize* a value from a starting range then *project* it into another range
+    var hourFill1 = map(now.hour, 1, fill('darkslategrey')) // from hours (1-12) to pixels (0–maxWidth)
+    var hourFill2 = map(now.hour, 1,12, 0,maxWidth) // from hours (1-12) to pixels (0–maxWidth)
+    var minsFill1 = map(now.min,  0,60, 0,maxWidth)  // from mins (0–60) to pixels (0–maxWidth)
+    var minsFill2 = map(now.min,  0,60, 0,maxWidth)  // from mins (0–60) to pixels (0–maxWidth)
+    var secsFill1 = map(now.sec,  0,60, 0,maxWidth)  // from secs (0–60) to pixels (0–maxWidth)
+    var secsFill2 = map(now.sec,  0,60, 0,maxWidth)  // from secs (0–60) to pixels (0–maxWidth)
+  }
 
 function draw() {
   // check the clock for the current time and unpack some of its fields to generate a time-string
   var now = clock()
 
-  // set the background to 'white' – you can also specify colors use integers, hex-color strings and more.
-  // note that setting the background also clears the canvas from our previous round of drawing
-  background('white')
+  
+// draw and fill the 6 rectangles
+// hour 1
+fill(255, 204, 0);
+rect(0, 0, 100, 300);
+// hour 2
+fill(250, 04, 10);
+rect(100, 0, 100, 300);
+//minute 1
+fill(200, 104, 12);
+rect(200, 0, 100, 300);
+//minute 2
+fill(50, 14, 200);
+rect(300, 0, 100, 300);
+//second 1
+fill(240, 54, 10);
+rect(400, 0, 100, 300);
+//second 2
+fill(50, 76, 100);
+rect(500, 0, 100, 300);
 
   // set up typography & drawing-color
-  textFont("Anonymous Pro") // ← check index.html to see how it was loaded from google-fonts
-  textSize(42) // make it big
-  fill(100, 50, 50)
+  // textFont("Anonymous Pro") // ← check index.html to see how it was loaded from google-fonts
+  // textSize(42) // make it big
+  // fill(100, 50, 50)
 
   // draw the time string to the canvas
-  text(now.text.date, 30, 50)
-  text(now.text.time, 30, 100)
+  // text(now.text.date, 30, 50)
+  // text(now.text.time, 30, 100)
 
 }
