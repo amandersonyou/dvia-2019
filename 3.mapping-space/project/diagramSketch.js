b4d77485868e2f4b287e49b64e39fbc6be4bdafd
EC2 Default User <ec2-user@ip-172-31-23-62.ec2.internal>
2019-12-02 18:07:40 +0000

    updates

--- 3.mapping-space/project/diagramSketch.js
+++ 3.mapping-space/project/diagramSketch.js
@@ -1,33 +1,33 @@
-var allDay
-var station
-
+var sigMonth;
+var station;
 
 // load the csv files 
 function preload(){
-  allDay = loadTable('data/all_day.csv', 'csv', 'header')
-  station = loadTable('data/GNS_ANSSstations.csv', 'csv', 'header')
+  sigMonth = loadTable('../data/2.5_day.csv', 'csv', 'header');
+  station = loadTable('../data/GNS_ANSSstations.csv', 'csv', 'header');
 }
 
-function setup(){
-  createCanvas(1000, 1000, SVG)
+function setup() {
+  createCanvas(3000, 1000)
   background(0,148,153) // Viridian Green Pantone
 
   // create tables for each type of value to be used
-  var table = allDay
-  var table_station = station
+var table_sigMonth = sigMonth;
+var table_station = station;
 
 
 
-  // set up typography
-  textFont("Rokkitt")
-  textSize(18)
-  fill(30)
-  noStroke()
-  
-}
-
-
+var symbolWidth = 60
 
 // function draw() {
+  
+x = 50
+y = 300
+fill(222,152,171, 250)
+  for (var m=0; m<table_sigMonth.getRowCount(); m++){
+    var mag = table_sigMonth.getNum(m, 4)
+    ellipse(x+symbolWidth, y, mag*10)
+    x += symbolWidth
+  };
     
-// }
+};
