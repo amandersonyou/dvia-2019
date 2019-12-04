41595720e3f420225039d455664ce91a0babaed2
EC2 Default User <ec2-user@ip-172-31-23-62.ec2.internal>
2019-12-04 03:00:30 +0000

    updates

--- 3.mapping-space/project/diagramSketch.js
+++ 3.mapping-space/project/diagramSketch.js
@@ -1,33 +1,79 @@
-var sigMonth;
+var daydata;
 var station;
 
 // load the csv files 
 function preload(){
-  sigMonth = loadTable('../data/2.5_day.csv', 'csv', 'header');
+  daydata = loadTable('../data/2.5_day_edit.csv', 'csv', 'header');
   station = loadTable('../data/GNS_ANSSstations.csv', 'csv', 'header');
 }
 
+
+
 function setup() {
-  createCanvas(3000, 1000)
-  background(0,148,153) // Viridian Green Pantone
+  createCanvas(2800, 1200)
+}
 
   // create tables for each type of value to be used
-var table_sigMonth = sigMonth;
-var table_station = station;
 
 
-
-var symbolWidth = 60
-
-// function draw() {
+function draw() {
+  background(240,248,255) // Alice Blue
+  noStroke()
   
-x = 50
-y = 300
-fill(222,152,171, 250)
-  for (var m=0; m<table_sigMonth.getRowCount(); m++){
-    var mag = table_sigMonth.getNum(m, 4)
-    ellipse(x+symbolWidth, y, mag*10)
+  var table_daydata = daydata;
+  var table_station = station;
+  var symbolWidth = 65
+  
+  x = 65
+  y = 300
+  
+  // magnitude circles
+  fill(220, 20, 60) // Crimson
+  for (var m=0; m<table_daydata.getRowCount(); m++){
+    var mag = (table_daydata.getNum(m, 4)*10)
+    ellipse(x, y, mag)
     x += symbolWidth
   };
+  
+  x = 65
+  y = 300
+    
+  // depth rectangle aka line
+  // NEED HELP WITH STARTING LINE AT BOTTOM OF CIRCLE
+  fill(0,0,139) // dark blue
+  for (var d=0; d<table_daydata.getRowCount(); d++){
+    var depth = table_daydata.getNum(d, 3)
+    rect(x, y+(mag), 4, depth*1.5)
+    x += symbolWidth
+  };
+
+  x = 65
+  y = 300
+  
+  // depth error rectangle
+  fill(46,139,87,145) // sea green
+  for (var e=0; e<table_daydata.getRowCount(); e++){
+    var d_err = table_daydata.getNum(e, 13) // 16 with the real sheet
+    rect(x-7, y+(mag), 18, d_err*1.5)
+    x += symbolWidth
+  };
+  
+  x = 65
+  y = 300
+  // station distance
+    // depth lines  NEED TO FIX NAN CONTINUE
+  fill(255,140,0) // dark orange
+  for (var s=0; s<table_daydata.getRowCount(); s++){
+    // var row = table_daydata.getRow(s)
+    // if (row.get('dmin')==''){
+    //   continue
+    // }
+    var dmin = table_daydata.getNum(s, 5)
+    // console.log(dmin)
+    rect(x, y-(mag), 4, -dmin*10)
+    // stroke(10)
+    x += symbolWidth
+  };
+  
     
 };
