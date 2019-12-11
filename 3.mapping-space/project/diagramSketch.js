d87e2296cb88e0b6793aab8baa186007ba0cc522
EC2 Default User <ec2-user@ip-172-31-23-62.ec2.internal>
2019-12-11 03:53:42 +0000

    updates for final

--- 3.mapping-space/project/diagramSketch.js
+++ 3.mapping-space/project/diagramSketch.js
@@ -7,55 +7,69 @@ function preload(){
   station = loadTable('../data/GNS_ANSSstations.csv', 'csv', 'header');
 }
 
+// find distance between two places given the lats and longs
 function measureDistance(srcLat, srcLng, dstLat, dstLng){
   var origin = L.latLng(srcLat, srcLng),
       point = L.latLng(dstLat, dstLng)
-  return origin.distanceTo(point) / 1000 // in kilometers
+  return (origin.distanceTo(point) / 1000)
 }
 
 
 function setup() {
-  createCanvas(2800, 1200)
+  createCanvas(2750, 1500)
   
   // sort data by depth error
+  // daydata.rows= _.sortBy(daydata.rows, row => -row.getNum('stnDist'))
   daydata.rows= _.sortBy(daydata.rows, row => -row.getNum('depthError'))
+
   
-  // measure find closest station 
-  var quake = daydata.getRow(0);
+  // use measureDistance function to find distance from quake to its nearest station
+  // recorded these values into the daydata csv
+  var quake = daydata.getRow(40);
   var quakeLat = quake.getNum('latitude')
   var quakeLong = quake.getNum('longitude')
   var closestStations = _.sortBy(station.rows, row => measureDistance(quakeLat, quakeLong, row.getNum('Latitude'), row.getNum('Longitude')))
   var closest = closestStations[0];
-  print(quake)
-  print(closest)
-
+  var stationDistance = measureDistance(quakeLat, quakeLong, closest.getNum('Latitude'), closest.getNum('Longitude'))
+  // print(quake)
+  // print(closest)
+  // print(stationDistance)
 }
 
-  // create tables for each type of value to be used
 
-
+// draw shapes to represent the various data relating to the quakes and stations
 function draw() {
   background(240,248,255) // Alice Blue
-  noStroke()
+  // noStroke()
   
   var table_daydata = daydata;
   var table_station = station;
   var symbolWidth = 65
   
-  x = 65
-  y = 300
+
+  // stroke(20)
   
+  x = 65
+  y = 500
   // magnitude circles
-  fill(255, 79, 0, 250) // orange
+  fill(255, 173, 51) // orange
   for (var m=0; m<table_daydata.getRowCount(); m++){
     var mag = (table_daydata.getNum(m, 4)*10)
+    var magError = (table_daydata.getNum(m, 7)*10)
+    strokeWeight(magError)
+    // stroke(255, 230, 179)
+    stroke(255, 255, 179)
     ellipse(x, y, mag)
+    
+    
+    // strokeFill("red")
     x += symbolWidth
   };
   
+  noStroke()
+  
   x = 65
-  y = 350
-    
+  y = 550
   // depth rectangle aka line
   fill(72,61,139) // dark slate blue
   for (var d=0; d<table_daydata.getRowCount(); d++){
@@ -64,9 +78,9 @@ function draw() {
     x += symbolWidth
   };
 
+
   x = 65
-  y = 350
-  
+  y = 550
   // depth error rectangle
   fill(46,139,87,145) // sea green
   for (var e=0; e<table_daydata.getRowCount(); e++){
@@ -75,21 +89,30 @@ function draw() {
     x += symbolWidth
   };
   
-  x = 65
-  y = 250
+  x = 60
+  y = 450
+  /* dmin is defined as: horizontal distance from the epicenter to the nearest 
+  station (in degrees). 1 degree is approximately 111.2 kilometers. In general, 
+  the smaller this number, the more reliable is the calculated depth of the 
+  earthquake.*/
+  fill(218, 179, 255) // lavendar
+  for (var s=0; s<table_daydata.getRowCount(); s++){
+    var dmin = table_daydata.getNum(s, 5) // 6 with the real sheet
+    rect(x, y, 4, -dmin*111.2/5)
+    x += symbolWidth
+  };
+  
+  
+  x = 70
+  y = 450
   // station distance
     // depth lines  NEED TO FIX NAN CONTINUE
   fill(186,85,211) // medium orchid
   for (var s=0; s<table_daydata.getRowCount(); s++){
-    // var row = table_daydata.getRow(s)
-    // if (row.get('dmin')==''){
-    //   continue
-    // }
-    var dmin = table_daydata.getNum(s, 5) // 6 with the real sheet
-    rect(x, y, 4, -dmin*10)
+    var dmin = table_daydata.getNum(s, 8) // 
+    rect(x, y, 4, -dmin/5)
     x += symbolWidth
   };
-  
 
 };
 
--- 3.mapping-space/project/sketch.js
+++ 3.mapping-space/project/sketch.js
@@ -1,13 +1,20 @@
 // the data loaded from a USGS-provided CSV file
-var table;
+var daydata;
+var station;
 
 // my leaflet.js map
 var mymap;
 
 function preload() {
     // load the CSV data into our `table` variable and clip out the header row
-    table = loadTable("../data/2.5_day_edit.csv", "csv", "header");
-    table2 = loadTable("../data/GNS_ANSSstations.csv", "csv", "header");
+    daydata = loadTable("../data/2.5_day_edit.csv", "csv", "header");
+    station = loadTable("../data/GNS_ANSSstations.csv", "csv", "header");
+}
+
+function measureDistance(srcLat, srcLng, dstLat, dstLng){
+  var origin = L.latLng(srcLat, srcLng),
+      point = L.latLng(dstLat, dstLng)
+  return (origin.distanceTo(point) / 1000)
 }
 
 function setup() {
@@ -18,29 +25,112 @@ function setup() {
     addCircles();
 
     // generate a p5 diagram that complements the map, communicating the earthquake data non-spatially
-    createCanvas(800, 600)
-    background(222)
+    createCanvas(2750, 1100)
+    background(240,248,255)
+    
+    daydata.rows= _.sortBy(daydata.rows, row => -row.getNum('stnDist'))
+    // daydata.rows= _.sortBy(daydata.rows, row => -row.getNum('depthError'))
 
+    
+    // use measureDistance function to find distance from quake to its nearest station
+    // recorded these values into the daydata csv
+    var quake = daydata.getRow(40);
+    var quakeLat = quake.getNum('latitude')
+    var quakeLong = quake.getNum('longitude')
+    var closestStations = _.sortBy(station.rows, row => measureDistance(quakeLat, quakeLong, row.getNum('Latitude'), row.getNum('Longitude')))
+    var closest = closestStations[0];
+    var stationDistance = measureDistance(quakeLat, quakeLong, closest.getNum('Latitude'), closest.getNum('Longitude'))
+    // print(quake)
+    // print(closest)
+    // print(stationDistance)
+    
     fill(0)
     noStroke()
-    textSize(16)
-    text(`Plotting ${table.getRowCount()} seismic events`, 20, 40)
-    text(`Plotting ${table2.getRowCount()} monitoring stations`, 20, 60)
-    text(`Largest Magnitude: ${columnMax(table, "mag")}`, 20, 80)
-    text(`Greatest Depth: ${columnMax(table, "depth")}`, 20, 100)
+}
+
+function draw() {
+    
+  var table_daydata = daydata;
+  var table_station = station;
+  var symbolWidth = 65
+  
+
+  // stroke(20)
+  
+  x = 65
+  y = 450
+  // magnitude circles
+  fill(255, 173, 51) // orange
+  for (var m=0; m<table_daydata.getRowCount(); m++){
+    var mag = (table_daydata.getNum(m, 4)*10)
+    var magError = (table_daydata.getNum(m, 7)*10)
+    strokeWeight(magError)
+    // stroke(255, 230, 179)
+    stroke(255, 255, 179)
+    ellipse(x, y, mag)
+    
+    
+    // strokeFill("red")
+    x += symbolWidth
+  };
+  
+  noStroke()
+  
+  x = 65
+  y = 500
+  // depth rectangle aka line
+  fill(72,61,139) // dark slate blue
+  for (var d=0; d<table_daydata.getRowCount(); d++){
+    var depth = table_daydata.getNum(d, 3)
+    rect(x, y, 4, depth)
+    x += symbolWidth
+  };
+
+
+  x = 65
+  y = 500
+  // depth error rectangle
+  fill(169, 213, 190, 70) // sea green
+//   46,139,87
+	
+	192,213,169
+  for (var e=0; e<table_daydata.getRowCount(); e++){
+    var d_err = table_daydata.getNum(e, 6) // 16 with the real sheet
+    rect(x-7, y, 18, d_err*1.5)
+    x += symbolWidth
+  };
+  
+  x = 60
+  y = 400
+  /* dmin is defined as: horizontal distance from the epicenter to the nearest 
+  station (in degrees). 1 degree is approximately 111.2 kilometers. In general, 
+  the smaller this number, the more reliable is the calculated depth of the 
+  earthquake.*/
+  fill(218, 179, 255) // lavendar
+  for (var s=0; s<table_daydata.getRowCount(); s++){
+    var dmin = table_daydata.getNum(s, 5) // 6 with the real sheet
+    rect(x, y, 4, -dmin*111.2/5)
+    x += symbolWidth
+  };
+  
+  
+  x = 70
+  y = 400
+  // station distance
+    // depth lines  NEED TO FIX NAN CONTINUE
+  fill(186,85,211) // medium orchid
+  for (var s=0; s<table_daydata.getRowCount(); s++){
+    var dmin = table_daydata.getNum(s, 8) // 
+    rect(x, y, 4, -dmin/5)
+    x += symbolWidth
+  };
+
 }
 
 function setupMap(){
-    /*
-    LEAFLET CODE
-
-    In this case "L" is leaflet. So whenever you want to interact with the leaflet library
-    you have to refer to L first.
-    so for example L.map('mapid') or L.circle([lat, long])
-    */
-
+    
     // create your own map
-    mymap = L.map('quake-map').setView([51.505, -0.09], 3);
+    mymap = L.map('quake-map').setView([35.505, -0.09], 4);
 
     // load a set of map tiles – choose from the different providers demoed here:
     // https://leaflet-extras.github.io/leaflet-providers/preview/
@@ -55,17 +145,12 @@ function setupMap(){
 function addCircles(){
     // calculate minimum and maximum values for magnitude and depth
     var magnitudeMin = 0.0;
-    var magnitudeMax = columnMax(table, "mag");
+    var magnitudeMax = columnMax(daydata, "mag");
     console.log('magnitude range:', [magnitudeMin, magnitudeMax])
 
-    // var depthMin = 0.0;
-    // var depthMax = columnMax(table, "depth");
-    // console.log('depth range:', [depthMin, depthMax])
-    
-
     // step through the rows of the table and add a dot for each event
-    for (var i=0; i<table.getRowCount(); i++){
-        var row = table.getRow(i)
+    for (var i=0; i<daydata.getRowCount(); i++){
+        var row = daydata.getRow(i)
 
         // skip over any rows where the magnitude data is missing
         if (row.get('mag')==''){
@@ -74,37 +159,34 @@ function addCircles(){
 
         // create a new dot
         var circle = L.circle([row.getNum('latitude'), row.getNum('longitude')], {
-            color: '#ff8300',      // the dot stroke color
-            fillColor: '#ff4f00', // the dot fill color
-            fillOpacity: 0.35,  // use some transparency so we can see overlaps
+            color: '#FFFFB3',      // the dot stroke color
+            fillColor: '#FFAD33', // the dot fill color
+            fillOpacity: 0.65,  // use some transparency so we can see overlaps
             radius: row.getNum('mag') * 40000
         })
 
         // place the new dot on the map
         circle.addTo(mymap);
+        circle.bindPopup('Earthquake with a magnitude of: ' + row.getNum('mag'));
     }
 
 
 //////////////// 
     // step through the rows of the table and add a dot for each station
-    for (var i=0; i<table2.getRowCount(); i++){
-        var row = table2.getRow(i)
-
-        // // skip over any rows where the magnitude data is missing
-        // if (row.get('mag')==''){
-        //     continue
-        // }
+    for (var i=0; i<station.getRowCount(); i++){
+        var row = station.getRow(i)
 
         // create a new dot
         var circle = L.circle([row.getNum('Latitude'), row.getNum('Longitude')], {
-            color: '#ba44ba',      // the dot stroke color
+            color: '#ba55d3',      // the dot stroke color
             fillColor: '#ba55d3', // the dot fill color
-            fillOpacity: 0.25,  // use some transparency so we can see overlaps
+            fillOpacity: 0.65,  // use some transparency so we can see overlaps
             radius: 30000
         })
 
         // place the new dot on the map
         circle.addTo(mymap);
+        circle.bindPopup("I'm a seismic monitoring station in " + row.getString('Name'));
     }
 }
 
